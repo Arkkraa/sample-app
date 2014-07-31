8dca5f1694ada77e98cd88c2ea73d4815cc4739e
Arkadiy Kraminsky <akramins@calpoly.edu>
2014-07-30 21:36:38 -0700

    Add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Arkkra's Website"}
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Arkkra's | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not include in title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -24,7 +31,7 @@ describe "Static pages" do
 
     it "should have the title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Arkkra's | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +44,15 @@ describe "Static pages" do
 
     it "should have the title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Arkkra's | About")
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "Should have the title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
